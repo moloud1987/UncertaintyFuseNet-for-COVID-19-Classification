60bef74a38e5abccbbddb85f00109105d700ef55
Sina Qahremani <sinaqahremani@gmail.com>
2021-05-08 17:48:02 +0430

    New Updates to python files. Simple models notebook added

--- evaluation.py
+++ evaluation.py
@@ -2,24 +2,30 @@ import numpy as np
 from sklearn.metrics import accuracy_score, precision_score, recall_score
 
 
-def mode_robustness(x_test, y_test, model, std_coef):
+def mode_robustness(x_test, y_test, model, std_coef, number_prediction=200):
     result = []
     for std in std_coef:
-
-        y_class = y_test
+        print("std: {}".format(std))
         x_class = x_test
-        mean = np.mean(x_class) * 0
+        mean = 0
         sigma = np.std(x_class) * std
         gaussian = np.random.normal(mean, sigma, x_class.shape)
         x_class = x_class + gaussian
-        y_p_class = model.predict(x_class)
-        acc = accuracy_score(np.argmax(y_class, axis=1), np.argmax(y_p_class, axis=1))
-        precision = precision_score(np.argmax(y_class, axis=1), np.argmax(y_p_class, axis=1), average='weighted')
-        recall = recall_score(np.argmax(y_class, axis=1), np.argmax(y_p_class, axis=1), average='weighted')
-        F1 = (2 * precision * recall) / (precision + recall)
-        result.append([std, acc, precision, recall, F1])
 
-    return result
+        mc_predictions = []
+
+        for _ in range(number_prediction):
+            y_p_class = model.predict(x_class)
+            mc_predictions.append(y_p_class)
+
+        mc_ensemble_pred = np.array(mc_predictions).mean(axis=0).argmax(axis=1)
+        ensemble_acc = accuracy_score(y_test.argmax(axis=1), mc_ensemble_pred)
+        ensemble_precision = precision_score(y_test.argmax(axis=1), mc_ensemble_pred, average='weighted')
+        ensemble_recall = recall_score(y_test.argmax(axis=1), mc_ensemble_pred, average='weighted')
+        ensemble_F1 = (2 * ensemble_precision * ensemble_recall) / (ensemble_precision + ensemble_recall)
+
+        result = [ensemble_acc * 100, ensemble_precision * 100, ensemble_recall * 100, ensemble_F1 * 100]
+        print("[{:0.5f}, {:0.5f}, {:0.5f}, {:0.5f}]\n".format(*result))
 
 
 def evaluation(y_test, y_pred):
--- models.py
+++ models.py
@@ -4,7 +4,7 @@ from tensorflow.keras.layers import Input, Dense, Flatten, Dropout, BatchNormali
 from tensorflow.keras.layers import Conv2D, SeparableConv2D, MaxPool2D
 from tensorflow.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, EarlyStopping
 
-image_size = 150
+
 
 
 def get_dropout(input_tensor, rate, mc=False):
@@ -14,7 +14,8 @@ def get_dropout(input_tensor, rate, mc=False):
         return Dropout(rate=rate)(input_tensor)
 
 
-def fusion_model(mc, lr=0.00005):
+# Our Proposed Model:
+def fusion_model(mc, image_size=150, lr=0.00005):
     inputs = Input(shape=(image_size, image_size, 1))
     input2 = tf.stack([inputs, inputs, inputs], axis=3)[:, :, :, :, 0]
     vgg_model = tf.keras.applications.VGG16(weights='imagenet',
@@ -93,3 +94,195 @@ def fusion_model(mc, lr=0.00005):
     callbacks = [reduce_lr, es, mcheck]
 
     return model, callbacks
+
+
+# Simple CNN Model:
+def simple_cnn_model(mc, image_size=150, lr=0.00005):
+    inputs = Input(shape=(image_size, image_size, 1))
+
+    conv1 = Conv2D(filters=16, kernel_size=(3, 3), activation='relu', padding='same')(inputs)
+
+    conv2 = Conv2D(filters=16, kernel_size=(3, 3), activation='relu', padding='same')(conv1)
+    conv2 = MaxPool2D(pool_size=(2, 2))(conv2)
+
+    conv3 = Conv2D(filters=32, kernel_size=(3, 3), activation='relu', padding='same')(conv2)
+
+    conv4 = Conv2D(filters=32, kernel_size=(3, 3), activation='relu', padding='same')(conv3)
+    conv4 = BatchNormalization()(conv4)
+    conv4 = MaxPool2D(pool_size=(2, 2))(conv4)
+
+    conv5 = Conv2D(filters=64, kernel_size=(3, 3), activation='relu', padding='same')(conv4)
+    conv5 = BatchNormalization()(conv5)
+    conv5 = get_dropout(conv5, rate=0.2, mc=mc)
+
+    # FC layer
+    x = Flatten()(conv5)
+
+    x = Dense(units=128, activation='relu')(x)
+    x = get_dropout(x, rate=0.7, mc=mc)
+
+    x = Dense(units=64, activation='relu')(x)
+    x = get_dropout(x, rate=0.5, mc=mc)
+
+    # Output layer
+    output = Dense(3, activation='softmax')(x)
+
+    METRICS = [
+        tf.keras.metrics.Precision(name='precision'),
+        tf.keras.metrics.Recall(name='recall'),
+        tf.keras.metrics.AUC(name='auc')]
+
+    # Creating model and compiling
+    model = Model(inputs=inputs, outputs=output)
+    adam = tf.keras.optimizers.Adam(lr=lr)
+    model.compile(optimizer=adam, loss='categorical_crossentropy', metrics=['accuracy', METRICS])
+
+    # Callbacks
+    if mc:
+        mcheck = ModelCheckpoint('simple_cnn_model_covid_mc.h5', monitor='val_accuracy', mode='max', verbose=1,
+                                 save_best_only=True)
+    else:
+        mcheck = ModelCheckpoint('simple_cnn_model_covid_simple.h5', monitor='val_accuracy', mode='max', verbose=1,
+                                 save_best_only=True)
+    reduce_lr = ReduceLROnPlateau(monitor='val_accuracy', factor=0.8, verbose=1, patience=5)
+    es = EarlyStopping(monitor='val_accuracy', mode='max', verbose=0, patience=30)
+    callbacks = [reduce_lr, es, mcheck]
+
+    return model, callbacks
+
+
+# Multi-headed Model:
+def multi_headed_model(mc, image_size=150, lr=0.00001):
+    inputs = Input(shape=(image_size, image_size, 1))
+
+    conv1 = Conv2D(filters=16, kernel_size=(3, 3), activation='relu', padding='same')(inputs)
+    conv1 = Conv2D(filters=8, kernel_size=(3, 3), activation='relu', padding='same')(conv1)
+    conv1 = BatchNormalization()(conv1)
+    conv1 = MaxPool2D(pool_size=(2, 2))(conv1)
+    conv1 = get_dropout(conv1, rate=0.2, mc=mc)
+
+    conv2 = Conv2D(filters=16, kernel_size=(3, 3), activation='relu', padding='same')(inputs)
+    conv2 = Conv2D(filters=8, kernel_size=(3, 3), activation='relu', padding='same')(conv2)
+    conv2 = BatchNormalization()(conv2)
+    conv2 = MaxPool2D(pool_size=(2, 2))(conv2)
+    conv2 = get_dropout(conv2, rate=0.2, mc=mc)
+
+    conv3 = Conv2D(filters=16, kernel_size=(3, 3), activation='relu', padding='same')(inputs)
+    conv3 = Conv2D(filters=8, kernel_size=(3, 3), activation='relu', padding='same')(conv3)
+    conv3 = BatchNormalization()(conv3)
+    conv3 = MaxPool2D(pool_size=(2, 2))(conv3)
+    conv3 = get_dropout(conv3, rate=0.2, mc=mc)
+
+    concatenated_tensor = Concatenate(axis=1)([Flatten()(conv1), Flatten()(conv2), Flatten()(conv3)])
+
+    # FC layer
+    x = Flatten()(concatenated_tensor)
+    x = Dense(units=128, activation='relu')(x)
+
+    x = get_dropout(x, rate=0.7, mc=mc)
+    x = Dense(units=64, activation='relu')(x)
+    x = get_dropout(x, rate=0.5, mc=mc)
+
+    # Output layer
+    output = Dense(3, activation='softmax')(x)
+
+    METRICS = [
+        tf.keras.metrics.Precision(name='precision'),
+        tf.keras.metrics.Recall(name='recall'),
+        tf.keras.metrics.AUC(name='auc')]
+
+    # Creating model and compiling
+    model = Model(inputs=inputs, outputs=output)
+    adam = tf.keras.optimizers.Adam(lr=lr)
+    model.compile(optimizer=adam, loss='categorical_crossentropy', metrics=['accuracy', METRICS])
+
+    # Callbacks
+    if mc:
+        mcheck = ModelCheckpoint('multi_headed_model_covid_mc.h5', monitor='val_accuracy', mode='max', verbose=1,
+                                 save_best_only=True)
+    else:
+        mcheck = ModelCheckpoint('multi_headed_model_covid_simple.h5', monitor='val_accuracy', mode='max', verbose=1,
+                                 save_best_only=True)
+    reduce_lr = ReduceLROnPlateau(monitor='val_accuracy', factor=0.8, verbose=1, patience=5)
+    es = EarlyStopping(monitor='val_accuracy', mode='max', verbose=0, patience=30)
+    callbacks = [reduce_lr, es, mcheck]
+
+    return model, callbacks
+
+
+# Truncated Models Used in t-SNE:
+def simple_cnn_trunc_model(trained_model, mc, image_size=150, lr=0.00005):
+    inputs = Input(shape=(image_size, image_size, 1))
+
+    conv1 = Conv2D(filters=16, kernel_size=(3, 3), activation='relu', padding='same')(inputs)
+
+    conv2 = Conv2D(filters=16, kernel_size=(3, 3), activation='relu', padding='same')(conv1)
+    conv2 = MaxPool2D(pool_size=(2, 2))(conv2)
+
+    conv3 = Conv2D(filters=32, kernel_size=(3, 3), activation='relu', padding='same')(conv2)
+
+    conv4 = Conv2D(filters=32, kernel_size=(3, 3), activation='relu', padding='same')(conv3)
+    conv4 = BatchNormalization()(conv4)
+    conv4 = MaxPool2D(pool_size=(2, 2))(conv4)
+
+    conv5 = Conv2D(filters=64, kernel_size=(3, 3), activation='relu', padding='same')(conv4)
+    conv5 = BatchNormalization()(conv5)
+    conv5 = get_dropout(conv5, rate=0.2, mc=mc)
+
+    # Output layer
+    x = Flatten()(conv5)
+    x = Dense(units=128, activation='relu')(x)
+    output = x
+
+    # Creating model and compiling
+    model = Model(inputs=inputs, outputs=output)
+    adam = tf.keras.optimizers.Adam(lr=lr)
+    model.compile(optimizer=adam, loss='categorical_crossentropy')
+
+    for i, layer in enumerate(model.layers):
+        layer.set_weights(trained_model.layers[i].get_weights())
+
+    model.compile(optimizer=adam, loss='categorical_crossentropy')
+
+    return model
+
+
+def multi_headed_trunc_model(trained_model, mc, image_size=150, lr=0.00001):
+    inputs = Input(shape=(image_size, image_size, 1))
+
+    conv1 = Conv2D(filters=16, kernel_size=(3, 3), activation='relu', padding='same')(inputs)
+    conv1 = Conv2D(filters=8, kernel_size=(3, 3), activation='relu', padding='same')(conv1)
+    conv1 = BatchNormalization()(conv1)
+    conv1 = MaxPool2D(pool_size=(2, 2))(conv1)
+    conv1 = get_dropout(conv1, rate=0.2, mc=mc)
+
+    conv2 = Conv2D(filters=16, kernel_size=(3, 3), activation='relu', padding='same')(inputs)
+    conv2 = Conv2D(filters=8, kernel_size=(3, 3), activation='relu', padding='same')(conv2)
+    conv2 = BatchNormalization()(conv2)
+    conv2 = MaxPool2D(pool_size=(2, 2))(conv2)
+    conv2 = get_dropout(conv2, rate=0.2, mc=mc)
+
+    conv3 = Conv2D(filters=16, kernel_size=(3, 3), activation='relu', padding='same')(inputs)
+    conv3 = Conv2D(filters=8, kernel_size=(3, 3), activation='relu', padding='same')(conv3)
+    conv3 = BatchNormalization()(conv3)
+    conv3 = MaxPool2D(pool_size=(2, 2))(conv3)
+    conv3 = get_dropout(conv3, rate=0.2, mc=mc)
+
+    concatenated_tensor = Concatenate(axis=1)([Flatten()(conv1), Flatten()(conv2), Flatten()(conv3)])
+
+    # Output layer
+    x = Flatten()(concatenated_tensor)
+    x = Dense(units=128, activation='relu')(x)
+    output = x
+
+    # Creating model and compiling
+    model = Model(inputs=inputs, outputs=output)
+    adam = tf.keras.optimizers.Adam(lr=lr)
+    model.compile(optimizer=adam, loss='categorical_crossentropy')
+
+    for i, layer in enumerate(model.layers):
+        layer.set_weights(trained_model.layers[i].get_weights())
+
+    model.compile(optimizer=adam, loss='categorical_crossentropy')
+
+    return model
